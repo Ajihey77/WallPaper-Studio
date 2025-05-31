d1a8d3178cb3cf7f5906a9c40d2dcdd533792899
Ajihey77 <anjihey77@gmail.com>
2025-05-31 17:47:54 +0900

    기본개념이해

--- app/(main)/@preview/[id]/page.tsx
+++ app/(main)/@preview/[id]/page.tsx
@@ -6,6 +6,12 @@ export default async function ImagePreview({
 }: {
   params: { id: string };
 }) {
+  /**
+   * next는 기본적으로 정적처리를 하는데 이 신성한 정적처리에
+   * 값자기 동적처리를 하는 param을 가져오니까 next가 경고를 줌
+   * 이거 정적 처리맞아?
+   * 항상 정적 처리와 동적처리를 고려하기
+   * */
   const images = await getImg(params.id);
   return (
     <div>
