9c822f156dd763fd2be9b378b4c66fe33d919f55
Ajihey77 <anjihey77@gmail.com>
2025-10-29 18:11:56 +0900

    🔥 : 불필요 라인 제거

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,8 +1,5 @@
-import { Suspense } from "react";
 import MainScrollView from "../../ui/main-scroll-view";
-import SkeletonGrid from "../../ui/skeletongrid";
 import { getAllImg } from "../../lib/cloudinary";
-import { ErrorTest } from "../../components/error/ErrorTest";
 
 async function ImagesSection() {
   const images = await getAllImg(); // API 라우트 우회, 직접 호출
